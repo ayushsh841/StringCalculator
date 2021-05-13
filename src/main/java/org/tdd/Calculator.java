426a8dc663f9af3845d4293e93630b0ea3e55357
Ayush Sharma <rr519532@prscoad.com>
2021-05-13 22:26:33 +0530

    Kata-1 Test Case 3 Pass Refactor

--- src/main/java/org/tdd/Calculator.java
+++ src/main/java/org/tdd/Calculator.java
@@ -8,6 +8,10 @@ public class Calculator {
         if (numbers.isEmpty()) {
             sum = 0;
         }
+        else if (numbers.contains(",")) {
+            sum = Integer.parseInt(String.valueOf(numbers.charAt(0)))
+                    + Integer.parseInt(String.valueOf(numbers.charAt(2)));
+        }
         else {
             sum = Integer.parseInt(numbers);
         }
