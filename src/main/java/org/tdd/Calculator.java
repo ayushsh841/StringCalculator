ae462e97ef4fdf436beafa322e5af7ddfd4db281
Ayush Sharma <rr519532@prscoad.com>
2021-05-13 22:11:56 +0530

    Kata-1 Test Case 1 Pass Refactor

--- src/main/java/org/tdd/Calculator.java
+++ src/main/java/org/tdd/Calculator.java
@@ -3,6 +3,11 @@ package org.tdd;
 public class Calculator {
 
     public int Add(String numbers) {
+
+        if (numbers.isEmpty()) {
+            return 0;
+        }
+
         return -1;
     }
 }
