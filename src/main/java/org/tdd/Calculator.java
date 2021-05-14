619bd4617498d8952fc930565aeeb4d963c9b3fe
Ayush Sharma <rr519532@prscoad.com>
2021-05-14 17:14:59 +0530

    Kata-1 Part 5 Test Case Pass Refactor

--- src/main/java/org/tdd/Calculator.java
+++ src/main/java/org/tdd/Calculator.java
@@ -1,9 +1,11 @@
 package org.tdd;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+import java.util.stream.Collectors;
 
 public class Calculator {
 
@@ -19,22 +21,51 @@ public class Calculator {
             }
             else {
                 List<String> splitNums = GetSeparatedNumbers(numbers);
-                sum = GetSum(splitNums);
+                List<Integer> values = Convert(splitNums);
+                List<Integer> negValues = Filter(values);
+
+                if (negValues.size() > 0) {
+                    String numberString = negValues.stream().map(String::valueOf)
+                                          .collect(Collectors.joining(", "));
+
+                    throw new RuntimeException("negatives not allowed - " + numberString);
+                }
+                sum = GetSum(values);
             }
         }
 
         return sum;
     }
 
-    public static int GetSum(List<String> numbers) {
-        int sum = 0;
+    public static int GetSum(List<Integer> numbers) {
+        int total = 0;
+
+        for (Integer value : numbers)
+            total += value;
+
+        return total;
+    }
+
+    public static List<Integer> Filter(List<Integer> numbers) {
+        List<Integer> negativeValues = new ArrayList<Integer>();
+
+        for (Integer value : numbers) {
+            if(value < 0)
+                negativeValues.add(value);
+        }
+
+        return negativeValues;
+    }
+
+    private static List<Integer> Convert(List<String> numbers) {
+        List<Integer> values = new ArrayList<Integer>();
 
         for(String number : numbers) {
             if (!number.isEmpty())
-                sum += Integer.parseInt(number);
+                values.add(Integer.parseInt(number));
         }
 
-        return sum;
+        return values;
     }
 
     public static List<String> GetSeparatedNumbers(String numbers) {
