e685fbe556eb7f866a9e6eda64f7ee3bbd9b501e
Ayush Sharma <rr519532@prscoad.com>
2021-05-14 17:16:43 +0530

    Kata-1 Part 6

--- src/test/java/org/tdd/CalculatorTest.java
+++ src/test/java/org/tdd/CalculatorTest.java
@@ -228,4 +228,18 @@ public class CalculatorTest {
 
         assertEquals(expectedSum, result);
     }
+
+    //Given: I provide string as 1,1001
+    //When: I call the Add method
+    //Then: I get the sum as 1
+    @Test
+    public void Add_GivenStringAs_1_1001_ReturnsSumAs_1()
+    {
+        String input = "1,1001";
+        int expectedSum = 1;
+
+        int result = calculator.Add(input);
+
+        assertEquals(expectedSum, result);
+    }
 }
